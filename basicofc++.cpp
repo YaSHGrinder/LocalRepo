9ef6b58628f60b9ea392a4ab08ffc6197d15a1a8
Yash <Panchalyash1319@gmail.com>
2025-08-17 18:44:50 +0530

    Calculator

--- basicofc++.cpp
+++ basicofc++.cpp
@@ -1,10 +1,47 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main (){
-    cout<<"Hi My name is yash panchal";
-    int age ;
-    cin>>age;
-    cout<<"You are"<<age<<"years old";
+// console calculator program
 
+int main (){
+char op;
+double num1;
+double num2;
+double result;
+
+cout<<"*******************CALCULATOR*******************\n";
+
+    cout<< "Enter either (+,-,/,*)\n";
+    cin>>op;
+
+    cout<<"Enter number 1: ";
+    cin>>num1;
+
+    cout<<"Enter number 2: ";
+    cin>>num2;
+
+    switch(op){
+        case '+':
+        result = num1 + num2;
+        cout<<"result: "<< result <<'\n';
+        break;
+        case '-':
+        result = num1 - num2;
+        cout<<"result: "<< result <<'\n';
+        break;
+        case '/':
+        result = num1 / num2;
+        cout<<"result: "<< result <<'\n';
+        break;
+        case '*':
+        result = num1 * num2;
+        cout<<"result: "<< result <<'\n';
+        break;
+        default: cout<<"Please Enter a valid operator (+,-,/,*)";
+    }
+    cout<<"********************************************";
+
+
+    return 0;
 }
+
